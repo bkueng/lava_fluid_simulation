417cecca43ac6a93795d6c586a947201f600de51
Beat Küng <beat-kueng@gmx.net>
2014-11-15 11:03:57 +0100

    config: use lookup_dist as default for cell_size

--- src/main_class.cpp
+++ src/main_class.cpp
@@ -249,7 +249,7 @@ void CMain::processArgs()
 		/* read the config */
 		config.simulation_time = (dfloat)sim_node.attribute("simulation_time").as_double(60);
 		config.neighbor_lookup_dist = (dfloat)sim_node.attribute("lookup_dist").as_double(0.03);
-		config.cell_size = (dfloat)sim_node.attribute("cell_size").as_double(0.03);
+		config.cell_size = (dfloat)sim_node.attribute("cell_size").as_double(config.neighbor_lookup_dist);
 		config.num_y_cells = sim_node.attribute("num_y_cells").as_int(20);
 		config.k = (dfloat)sim_node.attribute("pressure_k").as_double(1000);
 		config.rho0 = (dfloat)sim_node.attribute("rho0").as_double(1000);
