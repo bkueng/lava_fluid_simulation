373060b2a4ac32d5371c7ba02138826974a79a2d
Beat Küng <beat-kueng@gmx.net>
2014-11-24 09:58:01 +0100

    video script: add start_frame parameter to specify first frame

--- scripts/video.sh
+++ scripts/video.sh
@@ -6,14 +6,18 @@ popd > /dev/null
 
 if [ "$#" -lt 1 -o "$#" -gt 3 ]
 then
-    echo "Usage: $0 <rendering_directory> [fps]"
-	echo "       rendering_directory eg: output/rendering/sample"
+    echo "Usage: $0 <rendering_directory> [fps [start_frame]]"
+	echo "       <rendering_directory> eg: output/rendering/sample"
+	echo "       fps: input frame rate (default = 25)"
+	echo "       start_frame: first input frame (default = 0)"
 	exit
 fi
 
 rendering_dir="$1"
 FPS=25
 [ "$#" -gt 1 ] && FPS=$2
+start_frame_params=""
+[ "$#" -gt 2 ] && start_frame_params="-start_number $3"
 
 output_dir="$script_dir/../output/videos"
 mkdir "$output_dir" &>/dev/null
@@ -29,7 +33,8 @@ file_name="$file_name$idx"
 
 
 # -crf 22: high quality constant bitrate
-ffmpeg -framerate "$FPS" -i "$rendering_dir"/out_%06d.tif -vf fps=30 -crf 22 \
+ffmpeg -framerate "$FPS" $start_frame_params -i "$rendering_dir"/out_%06d.tif \
+	-vf fps=30 -crf 22 \
 	-vcodec libx264 -threads 6 "$output_dir/$file_name$suffix"
 
 echo "Generated video: $output_dir/$file_name$suffix"
